7a550e2f0dcc96aecaec85197ae261ba1faca823
Toyin Ajayi <toyin.ajayi@sainsburys.co.uk>
2021-10-18 13:09:13 +0100

    add images dynamically from data

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 
 const BlogPost = ({
@@ -8,9 +9,18 @@ const BlogPost = ({
     mdx: { frontmatter, body },
   },
 }) => {
+  const image = getImage(frontmatter.hero_image);
+
   return (
     <Layout pageTitle={frontmatter.title}>
       <p>{frontmatter.date}</p>
+      <GatsbyImage image={image} alt={frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={frontmatter.hero_image_credit_link}>
+          {frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{body}</MDXRenderer>
     </Layout>
   );
@@ -19,11 +29,19 @@ const BlogPost = ({
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
+      body
       frontmatter {
         title
         date
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `;
